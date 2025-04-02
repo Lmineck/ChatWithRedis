0d6bdabb58be333afc0785c36b95fe0fbd6ee9ef
MINHYEOK LEE <imhfox3573@it-cous.com>
2025-04-02 18:33:22 +0900

    add ChatMessage, RedisConfig

--- src/main/java/org/chatwithredis/config/RedisConfig.java
+++ src/main/java/org/chatwithredis/config/RedisConfig.java
@@ -1,4 +1,68 @@
 package org.chatwithredis.config;
 
+import org.chatwithredis.redis.RedisSubscriber;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.listener.*;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+
+    private final RedisSubscriber redisSubscriber;
+
+    /**
+     * Redis 연결 팩토리 (Lettuce 사용)
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    /**
+     * Redis 메시지 리스너 컨테이너 (구독용)
+     */
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(messageListenerAdapter(), topic());
+        return container;
+    }
+
+    /**
+     * 메시지를 수신할 리스너 어댑터
+     */
+    @Bean
+    public MessageListenerAdapter messageListenerAdapter() {
+        return new MessageListenerAdapter(redisSubscriber, "onMessage");
+    }
+
+    /**
+     * Redis Publish/Subscribe 주제 (채팅 채널)
+     */
+    @Bean
+    public ChannelTopic topic() {
+        return new ChannelTopic("chatroom");
+    }
+
+    /**
+     * RedisTemplate - String 기반으로 설정
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // 키와 값 모두 문자열 직렬화
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
 }
--- src/main/java/org/chatwithredis/model/ChatMessage.java
+++ src/main/java/org/chatwithredis/model/ChatMessage.java
@@ -1,4 +1,22 @@
 package org.chatwithredis.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessage {
+
+    public enum MessageType {
+        ENTER, TALK, LEAVE
+    }
+
+    private MessageType type;  // 메시지 타입 (입장, 채팅, 퇴장)
+    private String roomId;     // 채팅방 ID
+    private String sender;     // 보낸 사람
+    private String message;    // 실제 메시지 내용
 }
